1b98a947018555a721205b879d8aa2862b0e59ab
Konrad Nedzynski <nedzynski.k@gmail.com>
2018-11-12 23:18:07 +0100

    poprawki do maina. Kod buduje się z ecg_baseline. Brak bibliotek

--- EcgApplicationCore/main.cpp
+++ EcgApplicationCore/main.cpp
@@ -1,17 +1,15 @@
 #include "themewidget.h"
-#include "spline.h"
 #include "ecg_baseline.h"
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
 
 int main(int argc, char *argv[])
 {
-    tk::spline s;
     //module1::Ecg_Baseline ecg_baseline;
     QApplication a(argc, argv);
     QMainWindow window;
     ThemeWidget *widget = new ThemeWidget();
-    //module1::Ecg_Baseline *baseline = new module1::Ecg_Baseline();
+    //module1::Ecg_Baseline *baseline = new module1::Ecg_Baseline(); // tutaj cos z deklaracja namespace mozliwe ze usuniemy to module1 ale to do sprawdzenia
     window.setCentralWidget(widget);
     window.resize(900, 600);
     window.show();
