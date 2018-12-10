4c0162d5ab584197830ecd0a5115df026e9e9c58
DESKTOP-OF7S996\Maria <barnowskamaria@gmail.com>
2018-12-10 21:36:21 +0100

    wybor metody filtracji

--- EcgApplicationCore/View/ecgbaseline_gui.cpp
+++ EcgApplicationCore/View/ecgbaseline_gui.cpp
@@ -23,21 +23,21 @@ void ECGbaseline_gui::on_comboBox_filter_currentTextChanged(const QString &arg1)
       ui->spinBox_filOrder->setDisabled(true);
       ui->spinBox_filLength->setDisabled(true);
       ui->spinBox_stepSize->setDisabled(true);
-      ui->plainTextEdit->setPlainText("aloha");
+
     } else if (filter=="bandpass_filter") {
         ui->spinBox_lowFreq->setDisabled(false);
         ui->spinBox_highFreq->setDisabled(false);
         ui->spinBox_filOrder->setDisabled(true);
         ui->spinBox_filLength->setDisabled(true);
         ui->spinBox_stepSize->setDisabled(true);
-        ui->plainTextEdit->setPlainText("we did it1");
+
     } else if (filter=="Butterworth_filter") {
         ui->spinBox_lowFreq->setDisabled(false);
         ui->spinBox_highFreq->setDisabled(true);
         ui->spinBox_filOrder->setDisabled(false);
         ui->spinBox_filLength->setDisabled(true);
         ui->spinBox_stepSize->setDisabled(true);
-        ui->plainTextEdit->setPlainText("we did it2");
+
     } else if (filter=="Chebyshev_filter") {
         ui->spinBox_lowFreq->setDisabled(false);
         ui->spinBox_highFreq->setDisabled(false);
@@ -58,20 +58,20 @@ void ECGbaseline_gui::on_comboBox_filter_currentTextChanged(const QString &arg1)
         ui->spinBox_filOrder->setDisabled(true);
         ui->spinBox_filLength->setDisabled(true);
         ui->spinBox_stepSize->setDisabled(true);
-        ui->plainTextEdit->setPlainText("we did it5");
+
     } else if (filter=="LSM_algoritm") {
         ui->spinBox_lowFreq->setDisabled(true);
         ui->spinBox_highFreq->setDisabled(true);
         ui->spinBox_filOrder->setDisabled(true);
         ui->spinBox_filLength->setDisabled(false);
         ui->spinBox_stepSize->setDisabled(true);
-        ui->plainTextEdit->setPlainText("we did it6");
+
     }else if (filter=="Moving_average"){
         ui->spinBox_lowFreq->setDisabled(true);
         ui->spinBox_highFreq->setDisabled(true);
         ui->spinBox_filOrder->setDisabled(true);
         ui->spinBox_filLength->setDisabled(false);
         ui->spinBox_stepSize->setDisabled(false);
-        ui->plainTextEdit->setPlainText("ooop");
+
     }
 }
