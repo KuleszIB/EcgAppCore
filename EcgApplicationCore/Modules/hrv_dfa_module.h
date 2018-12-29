7419a2d801cbf61f3762fc2c3d0728fe8311b6ca
Behorsed <paulina.e.kowalik@wp.pl>
2018-12-29 19:33:36 +0100

    zmienione nazwy na angielskie

--- EcgApplicationCore/Modules/hrv_dfa_module.h
+++ EcgApplicationCore/Modules/hrv_dfa_module.h
@@ -3,35 +3,35 @@
 #include "r_peaks_module.h"
 #include "armadillo"
 
-struct Wyjsciowe_dane_DFA { // do wykresow potrzebne rzeczy
+struct Out_DFA { // do wykresow potrzebne rzeczy
     double alpha1; // parametr alfa analizy krotkoczasowej
     double alpha2; // parametr alfa analizy dlugoczasowej
     arma::vec log_F; // wektor logarytmiczny F do wykresu: wartości na osi Y na wykresie (punkty)
     arma::vec log_delta; // wektor logarytmiczny delta do wykresu: wartości na osi  X na wykresie
-    arma::vec prosta_krotkie_okna; // do wykresu: wartości Y dopasowanej prostej dla krotkich okien;
-    arma::vec prosta_dlugie_okna; // do wykresu: wartosci Y dopasowanej prostej dla dlugich okien;
+    arma::vec vector_short_windows; // do wykresu: wartości Y dopasowanej prostej dla krotkich okien;
+    arma::vec vector_long_windows; // do wykresu: wartosci Y dopasowanej prostej dla dlugich okien;
 };
 
 class HrvDfa : public R_Peaks
 {
 private:
-    arma::vec najmniejsze_kwadraty(arma::vec tm, arma::vec ym, int delta_m);
-    double oblicz_F(arma::vec tk, arma::vec x, int delta_m);
-    void zrob_tachogram();
-    Wyjsciowe_dane_DFA wyjsciowe_dane_DFA;
-    Wyjsciowe_dane_DFA oblicz_dane_DFA();
+    arma::vec least_squares(arma::vec tm, arma::vec ym, int delta_m);
+    double calculate_F(arma::vec tk, arma::vec x, int delta_m);
+    void make_tachogram();
+    Out_DFA out_DFA;
+    Out_DFA calculate_DFA();
     arma::vec cum_time_vec;
     arma::vec RR_intervals;
     arma::vec r_peaks;
     int middle;
-    int pierwsza_delta;
-    int ostatnia_delta;
+    int first_delta;
+    int last_delta;
 public:
     HrvDfa();
     HrvDfa(arma::vec r_peaks);
     // jesli chcemy sami wpisac srodek analizy czasowej (middle), najmniejsze okienko (pierwsza_delta), najwieksze okienko (ostatnia_delta)
-    HrvDfa(arma::vec r_peaks, int middle, int pierwsza_delta, int ostatnia_delta);
-    Wyjsciowe_dane_DFA get_wyjsciowe_dane_DFA();
+    HrvDfa(arma::vec r_peaks, int middle, int first_delta, int last_delta);
+    Out_DFA get_out_DFA();
 
 };
 
