de8cd6c784a1b6fa5159c32d1547339b2f070e14
DESKTOP-OF7S996\Maria <barnowskamaria@gmail.com>
2019-01-19 10:50:04 +0100

    wyłączenie edytowania pól tekstowych, w których będą tylko wyświetlane dane wyjściowe, aktualizacja nazw parametrów w HRV1 i HRV2, pobieranie wartości z spinboxów w STsegment i wpisywanie ich do tabeli

--- EcgApplicationCore/View/hrv_dfa_gui.cpp
+++ EcgApplicationCore/View/hrv_dfa_gui.cpp
@@ -1,5 +1,6 @@
 #include "hrv_dfa_gui.h"
 #include "ui_hrv_dfa_gui.h"
+#include "Modules/hrv_dfa_module.h"
 
 HRV_dfa_gui::HRV_dfa_gui(QWidget *parent) :
     QWidget(parent),
@@ -11,4 +12,17 @@ HRV_dfa_gui::HRV_dfa_gui(QWidget *parent) :
 HRV_dfa_gui::~HRV_dfa_gui()
 {
     delete ui;
+}
+
+void HRV_dfa_gui::on_pushButton_clicked()
+{
+    int deltaMaxValue = ui->deltaMax->value();
+    int deltaMinValue = ui->deltaMin->value();
+    QTableWidgetItem *theItem = new QTableWidgetItem();
+    theItem->setData(Qt::EditRole, deltaMaxValue);
+    ui->tableWidget->setItem(1, 0, theItem);
+    QTableWidgetItem *theItem1 = new QTableWidgetItem();
+    theItem1->setData(Qt::EditRole, deltaMinValue);
+    ui->tableWidget->setItem(1, 1, theItem1);
+
 }
--- EcgApplicationCore/View/hrv_dfa_gui.h
+++ EcgApplicationCore/View/hrv_dfa_gui.h
@@ -2,6 +2,10 @@
 #define HRV_DFA_H
 
 #include <QWidget>
+#include "ecgplot.h"
+#include "Modules/hrv_dfa_module.h"
+#include <QVBoxLayout>
+#include "Modules/examination.h"
 
 namespace Ui {
 class HRV_dfa_gui;
@@ -15,8 +19,12 @@ public:
     explicit HRV_dfa_gui(QWidget *parent = 0);
     ~HRV_dfa_gui();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::HRV_dfa_gui *ui;
+
 };
 
 #endif // HRV_DFA_H
