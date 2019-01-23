79a32a7d7b624fae3e98891d3f664ed77d9172c6
ksamojeden <kasiasamo1@gmail.com>
2019-01-23 18:51:30 +0100

    Dodane komentarze do R_Peaks_Waves

--- EcgApplicationCore/Modules/r_peaks_module.h
+++ EcgApplicationCore/Modules/r_peaks_module.h
@@ -6,23 +6,22 @@
 class R_Peaks : public Ecg_Baseline
 {
 private:
-    void pan_tompkins();
-    void filter_highpass(double fc, int M);
-    void square();
-    void integrate(int W);
-
+    void pan_tompkins();                    // Funkcja realizujaca algorytm Pan-Tompkins
+    void filter_highpass(double fc, int M); // Funkcja realizujaca filtracje gornoprzepustowa
+    void square();                          // Funkcja realizujaca potegowanie sygnalu
+    void integrate(int W);                  // Funkcja realizujaca calkowanie sygnalu
 
 protected:
-    void differentiate();
-    arma::vec find_peaks(arma::vec signal);
-    arma::vec r_peaks_vec; // output
+    void differentiate();                   // Funkcja realizujaca roznickzowanie sygnalu
+    arma::vec find_peaks(arma::vec signal); // Funkcja szukajaca maksimow
+    arma::vec r_peaks_vec;                  // Wektor znalezionych zalamkow R
 
 public:
-    R_Peaks();
-    R_Peaks(arma::vec signal, double fs=360);
-    void find_r_peaks();
-    arma::vec get_r_peaks();
-    void set_r_peaks(arma::vec new_r_peaks);
+    R_Peaks();                                  // Konstruktor 1
+    R_Peaks(arma::vec signal, double fs=360);   // Konstruktor 2
+    void find_r_peaks();                        // Funkcja szukajaca zalamkow R
+    arma::vec get_r_peaks();                    // Funkcja zwracajaca znalezione zalamki R
+    void set_r_peaks(arma::vec new_r_peaks);    // Funkcja ustawiajaca nowa wartosc zalamkow R
 };
 
 #endif // R_PEAKS_H
